e79c6d5e0bf089d4e7b87292fcd00cd4ee49944e
Ruan Carvalho <86540031+Ruansmc@users.noreply.github.com>
2024-01-23 22:32:17 -0300

    Finalização do Projeto

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = parseInt(Math.random() * 10 + 1);
+let numeroMaximo = 3000;
+let numeroSecreto = parseInt(Math.random() * 3000 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 //enquanto chute não for igual ao número secreto
 while (chute != numeroSecreto) {
-  chute = prompt("Escolha um número entre 1 e 10");
+  chute = prompt("Escolha um número entre 1 e 3000");
   // se chute for igual ao número secreto
   if (chute == numeroSecreto) {
     break;
@@ -26,6 +27,8 @@ let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
 alert(
   `Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`
 );
+
+//Comentários das atividades anteriores:
 // if (tentativas > 1) {
 //   alert(
 //     `Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`
